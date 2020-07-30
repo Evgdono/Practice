afbc7d51854ca0ce775acee27e30904c84e9832a
Евгений Черкасов <evg.seconddono@gmail.com>
2020-07-30 16:33:44 +0700

    Создание модели пользователя

---
+++ Practice.BL/Model/Gender.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Practice.BL.Model
+{
+    /// <summary>
+    /// Пол.
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Имя.
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Создать новый пол.
+        /// </summary>
+        /// <param name="name"></param>
+        public Gender(string name)
+        {
+            if(string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пола не может быть пустым или null.", nameof(name));
+            }
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
